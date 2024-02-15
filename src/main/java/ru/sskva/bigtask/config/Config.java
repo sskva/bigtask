ea82a44b02ffa90ef61141105e1eca575280b68e
ssk11 <ssk29@yandex.ru>
2024-02-15 17:14:40 +0300

    job

--- src/main/java/ru/sskva/bigtask/config/Config.java
+++ src/main/java/ru/sskva/bigtask/config/Config.java
@@ -9,5 +9,5 @@ public class Config {
 
     private int minCountLines = 1;
     private int maxCountLines = 100000;
-    private String urlExternalService = "http://localhost:8081/internal-service/getStatus/";
+    private String urlExternalService = "http://localhost:8081/external-service/getStatus/";
 }
